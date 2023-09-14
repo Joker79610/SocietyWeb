72290d079bcae18cb3d0a0245026e8fb7eda3827
qngg <124476435+qngg@users.noreply.github.com>
2023-09-14 10:24:37 +0000

    pls work pls work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import MarkdownIt from 'markdown-it';
-var md = new MarkdownIt();
-var result = md.render('# markdown-it rulezz!');
+import md from 'markdown-it'
+var singleline = md.renderInline('__markdown-it__ rulezz!'); //single line
 
-document.getElementById("test1").innerHTML = result;
+document.getElementById("test1").innerHTML = singleline;
+
+var multiline = md.render('# markdown-it rulezz!');
+
+document.getElementById().innerHTML = multiline;
